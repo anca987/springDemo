b04ae19a2e1cb4449a116f9adc66a436da6af901
Anca Gavrilescu <ankagavrilescu@gmail.com>
2018-04-16 23:49:22 +0300

    property initialization

--- src/org/beanfactory/DrawingApplication.java
+++ src/org/beanfactory/DrawingApplication.java
@@ -1,16 +1,17 @@
 package org.beanfactory;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApplication {
 
 	public static void main(String[] args) {
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-
-		Triangle triangle = (Triangle) factory.getBean("triangle");
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
+		System.out.println(triangle.getType());
 	}
 
 }
--- src/org/beanfactory/Triangle.java
+++ src/org/beanfactory/Triangle.java
@@ -2,8 +2,18 @@ package org.beanfactory;
 
 public class Triangle {
 
+	private String type;
+
 	public void draw() {
 		System.out.println("Drawing a triangle...");
 	}
-	
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
