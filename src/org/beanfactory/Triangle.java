41fb9154e784e151212f3378dbbd578d5e165b62
Anca Gavrilescu <ankagavrilescu@gmail.com>
2018-04-21 18:58:31 +0300

    collections

--- src/org/beanfactory/Triangle.java
+++ src/org/beanfactory/Triangle.java
@@ -1,39 +1,22 @@
 package org.beanfactory;
 
+import java.util.List;
+
 public class Triangle {
 
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
+	private List<Point> points;
+
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
 
 	public void draw() {
-		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
-		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
-		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+		for (Point point : points) {
+			System.out.println("Point A = (" + point.getX() + ", " + point.getY() + ")");
+		}
 	}
-
-	public Point getPointA() {
-		return pointA;
-	}
-
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-
-	public Point getPointB() {
-		return pointB;
-	}
-
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
-
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
-	}
-
 }
