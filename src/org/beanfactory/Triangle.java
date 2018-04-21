7435b3b3d5008a848ccf5bc2a0d36de7436a00f8
Anca Gavrilescu <ankagavrilescu@gmail.com>
2018-04-21 19:13:59 +0300

    autowire byName

--- src/org/beanfactory/Triangle.java
+++ src/org/beanfactory/Triangle.java
@@ -1,22 +1,38 @@
 package org.beanfactory;
 
-import java.util.List;
-
 public class Triangle {
 
-	private List<Point> points;
-
-	public List<Point> getPoints() {
-		return points;
-	}
-
-	public void setPoints(List<Point> points) {
-		this.points = points;
-	}
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
 	public void draw() {
-		for (Point point : points) {
-			System.out.println("Point A = (" + point.getX() + ", " + point.getY() + ")");
-		}
+		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point A = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point A = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+	}
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 }
