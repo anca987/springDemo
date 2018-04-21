21a87210ec48c39aedea7e38c9af0e1a3f1dd0c7
Anca Gavrilescu <ankagavrilescu@gmail.com>
2018-04-21 18:52:38 +0300

    inner beans and alias

--- src/org/beanfactory/Triangle.java
+++ src/org/beanfactory/Triangle.java
@@ -7,8 +7,9 @@ public class Triangle {
 	private Point pointC;
 
 	public void draw() {
-		System.out.println("Point A = (" + getPointA() + ")" + ", Point B = ("
-				+ getPointB() + ")" + ", Point C = (" + getPointC() + ")");
+		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 
 	public Point getPointA() {
