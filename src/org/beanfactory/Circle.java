03cdc648de6dc84a5100228019c3a74cb8d36093
Anca Gavrilescu <ankagavrilescu@gmail.com>
2018-04-22 11:20:26 +0300

    @Resource, @PostConstruct, @PreDestroy annotations

--- src/org/beanfactory/Circle.java
+++ src/org/beanfactory/Circle.java
@@ -1,7 +1,8 @@
 package org.beanfactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 
 public class Circle implements Shape {
 
@@ -11,8 +12,7 @@ public class Circle implements Shape {
 		return center;
 	}
 
-	@Autowired
-	@Qualifier("circleRelated")
+	@Resource(name="pointC")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
@@ -22,5 +22,14 @@ public class Circle implements Shape {
 		System.out.println("Drawing Circle");
 		System.out.println("Circle: Point is: (" + center.getX() + ", " + center.getY() + ")");
 	}
+	
+	@PostConstruct
+	public void initializeCircle() {
+		System.out.println("Init of Circle");
+	}
 
+	@PreDestroy
+	public void destroyCircle() {
+		System.out.println("Destroy of Circle");
+	}
 }
--- src/org/beanfactory/DrawingApplication.java
+++ src/org/beanfactory/DrawingApplication.java
@@ -1,13 +1,14 @@
 package org.beanfactory;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		
 		Shape shape = (Shape) context.getBean("circle");
 		shape.draw();
