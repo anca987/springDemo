ca76d842e68e25fdceed2a4bc875c6f5e8310040
Anca Gavrilescu <ankagavrilescu@gmail.com>
2018-04-22 12:21:28 +0300

    using MessageSource interface

--- src/org/beanfactory/Circle.java
+++ src/org/beanfactory/Circle.java
@@ -4,13 +4,17 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Circle implements Shape {
 
 	private Point center;
-
+	@Autowired
+	private MessageSource messageSource; 
+	
 	public Point getCenter() {
 		return center;
 	}
@@ -19,11 +23,20 @@ public class Circle implements Shape {
 	public void setCenter(Point center) {
 		this.center = center;
 	}
+	
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@Override
 	public void draw() {
-		System.out.println("Drawing Circle");
-		System.out.println("Circle: Point is: (" + center.getX() + ", " + center.getY() + ")");
+		System.out.println(this.messageSource.getMessage("drawing.circle", null, "Default Drawing Message", null));
+		System.out.println(this.messageSource.getMessage("drawing.point", new Object[] {center.getX(), center.getY()}, "Default Drawing Message", null));
+		//System.out.println("Greeting: " + this.messageSource.getMessage("greeting", null, "Default Greeting", null));
 	}
 	
 	@PostConstruct
